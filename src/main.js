1d9ab6591ba718926ff878ea23aea05e21c9e20b
Saveliy <miheev.saveliy.2002@gmail.com>
2023-09-23 09:19:35 +0800

    haha 2

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import { createApp } from "vue";
 import App from "./App";
 import components from "@/components/UI";
 import router from "@/router/router";
+import directive from "@/directives/index";
 
 const app = createApp(App);
 // Регистрация компонентов
 components.forEach((component) => {
   app.component(component.name, component);
+});
+directive.forEach((directive) => {
+  app.directive(directive.name, directive);
 });
 
 app.use(router).mount("#app");
--- src/pages/PostPage.vue
+++ src/pages/PostPage.vue
@@ -1,41 +1,50 @@
 <template>
-  <div class="app__btns">
+  <div>
     <h1>Страница с постами</h1>
-    <my-input v-model="searchQuery" placeholder="Поиск..." />
-    <my-button @click="showDialog">Создать пост</my-button>
-    <my-select v-model="selectedSort" :options="sortOptions"></my-select>
+    <my-input v-model="searchQuery" v-focus placeholder="Поиск...." />
+    <div class="app__btns">
+      <my-button @click="showDialog"> Создать пользователя </my-button>
+      <my-select v-model="selectedSort" :options="sortOptions" />
+    </div>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form @create="createPost" />
+    </my-dialog>
+    <post-list
+      :posts="sortedAndSearchedPosts"
+      @remove="removePost"
+      v-if="!isPostsLoading"
+    />
+    <div v-else>Идет загрузка...</div>
+    <div v-intersection="loadMorePosts" class="observer"></div>
+    <!--    <div class="page__wrapper">-->
+    <!--      <div-->
+    <!--        v-for="pageNumber in totalPages"-->
+    <!--        :key="pageNumber"-->
+    <!--        class="page"-->
+    <!--        :class="{-->
+    <!--          'current-page': page === pageNumber-->
+    <!--        }"-->
+    <!--        @click="changePage(pageNumber)"-->
+    <!--      >-->
+    <!--        {{ pageNumber }}-->
+    <!--      </div>-->
+    <!--    </div>-->
   </div>
-
-  <my-dialog v-model:show="dialogVisible">
-    <post-form @create="createPost"
-  /></my-dialog>
-
-  <post-list
-    :posts="sortedAndSearchedPosts"
-    @remove="reportPost"
-    v-if="!isPostsLoading"
-  />
-  <div v-else>Идет загрузка...</div>
-  <div ref="observer" class="observer"></div>
-  <!-- <div class="page__wrapper">
-      <div
-        v-for="pageNumber in totalPages"
-        :key="pageNumber"
-        class="page"
-        :class="{ 'current-page': page === pageNumber }"
-        @click="changePage(pageNumber)"
-      >
-        {{ pageNumber }}
-      </div>
-    </div> -->
 </template>
 
 <script>
-import PostForm from "@/components/PostForm.vue";
-import PostList from "@/components/PostList.vue";
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
+import MyButton from "@/components/UI/MyButton";
 import axios from "axios";
+import MySelect from "@/components/UI/MySelect";
+import MyInput from "@/components/UI/MyInput";
+
 export default {
   components: {
+    MyInput,
+    MySelect,
+    MyButton,
     PostList,
     PostForm,
   },
@@ -50,39 +59,35 @@ export default {
       limit: 10,
       totalPages: 0,
       sortOptions: [
-        {
-          value: "title",
-          name: "По названию",
-        },
-        {
-          value: "body",
-          name: "По описанию",
-        },
+        { value: "title", name: "По названию" },
+        { value: "body", name: "По содержимому" },
       ],
     };
   },
-
   methods: {
     createPost(post) {
       this.posts.push(post);
+      this.dialogVisible = false;
     },
-    reportPost(post) {
+    removePost(post) {
       this.posts = this.posts.filter((p) => p.id !== post.id);
     },
     showDialog() {
       this.dialogVisible = true;
     },
     // changePage(pageNumber) {
-    //   this.page = pageNumber;
+    //   this.page = pageNumber
     // },
     async fetchPosts() {
       try {
         this.isPostsLoading = true;
-
         const response = await axios.get(
-          "https://jsonplaceholder.typicode.com/posts?",
+          "https://jsonplaceholder.typicode.com/posts",
           {
-            params: { _page: this.page, _limit: this.limit },
+            params: {
+              _page: this.page,
+              _limit: this.limit,
+            },
           }
         );
         this.totalPages = Math.ceil(
@@ -98,11 +103,13 @@ export default {
     async loadMorePosts() {
       try {
         this.page += 1;
-
         const response = await axios.get(
-          "https://jsonplaceholder.typicode.com/posts?",
+          "https://jsonplaceholder.typicode.com/posts",
           {
-            params: { _page: this.page, _limit: this.limit },
+            params: {
+              _page: this.page,
+              _limit: this.limit,
+            },
           }
         );
         this.totalPages = Math.ceil(
@@ -111,25 +118,22 @@ export default {
         this.posts = [...this.posts, ...response.data];
       } catch (e) {
         alert("Ошибка");
-      } finally {
       }
     },
   },
   mounted() {
     this.fetchPosts();
-
-    const options = {
-      rootMargin: "0px",
-      threshold: 1.0,
-    };
-
-    const callback = (entries, observer) => {
-      if (entries[0].isIntersecting && this.page < this.totalPages) {
-        this.loadMorePosts();
-      }
-    };
-    const observer = new IntersectionObserver(callback, options);
-    observer.observe(this.$refs.observer);
+    // const options = {
+    //   rootMargin: '0px',
+    //   threshold: 1.0
+    // }
+    // const callback = (entries, observer) => {
+    //   if (entries[0].isIntersecting && this.page < this.totalPages) {
+    //     this.loadMorePosts()
+    //   }
+    // };
+    // const observer = new IntersectionObserver(callback, options);
+    // observer.observe(this.$refs.observer);
   },
   computed: {
     sortedPosts() {
@@ -139,14 +143,14 @@ export default {
     },
     sortedAndSearchedPosts() {
       return this.sortedPosts.filter((post) =>
-        post.title.toLowerCase().includes(this.searchQuery.toLocaleLowerCase())
+        post.title.toLowerCase().includes(this.searchQuery.toLowerCase())
       );
     },
   },
   watch: {
     // page() {
-    //   this.fetchPosts();
-    // },
+    //   this.fetchPosts()
+    // }
   },
 };
 </script>
@@ -157,19 +161,20 @@ export default {
   display: flex;
   justify-content: space-between;
 }
-
 .page__wrapper {
   display: flex;
   margin-top: 15px;
 }
 .page {
   border: 1px solid black;
-  padding: 8px;
+  padding: 10px;
 }
 .current-page {
-  border: 3px solid green;
+  border: 2px solid teal;
 }
+
 .observer {
   height: 30px;
+  background: green;
 }
 </style>
