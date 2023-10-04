aa732bc4c93b77112385ffff32c189fe3beaeb54
s1riller <miheev.saveliy.2002@gmail.com>
2023-10-04 23:46:38 +0800

    Тут можно делать откат без имен

---
+++ src/components/RegisterForm.vue
@@ -0,0 +1,10 @@
+<template>
+  <form>
+    <h3>Sign Up</h3>
+
+    <div class="form-group">
+      <label>email</label>
+      <input type="email" class="form-control" placeholder="Email"  id="">
+    </div>
+  </form>
+</template>
